b83b02c14b61af740c50857301a20e488468062f
Andrew Philpot <andrew.philpot@gmail.com>
2015-12-14 16:58:55 -0800

    dict_minus drops a key and returns the dict

--- digSparkUtil/dictUtil.py
+++ digSparkUtil/dictUtil.py
@@ -10,3 +10,13 @@ def as_dict(likeDict):
             return likeDict.__dict__
         except:
             raise TypeError("Can't coerce {} to act like a dict".format(likeDict))
+
+def dict_minus(d, key):
+    """Delete key from dict if exists, returning resulting dict"""
+    # make shallow copy
+    d = dict(d)
+    try:
+        del d[key]
+    except:
+        pass
+    return d
