97e084c92c0360295980bc28ffa0b7463c82bf98
Andrew Philpot <andrew.philpot@gmail.com>
2015-12-14 16:05:36 -0800

    use ABC collections for more generality: anything which is like a dict will pass

--- digSparkUtil/dictUtil.py
+++ digSparkUtil/dictUtil.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+import collections
+
 def as_dict(likeDict):
-    if isinstance(likeDict, dict):
+    if isinstance(likeDict, collections.Mapping):
         return likeDict
     else:
         try:
