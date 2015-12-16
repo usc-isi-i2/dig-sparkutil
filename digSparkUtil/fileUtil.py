0effacc0d7c97d6e7b9c6eba0beab74cb4fdab56
Andrew Philpot <andrew.philpot@gmail.com>
2015-12-16 08:28:16 -0800

    spell fileformat as file_format, datatype as data_type everywhere pass file_format and data_type as kwargs

--- digSparkUtil/fileUtil.py
+++ digSparkUtil/fileUtil.py
@@ -14,23 +14,23 @@ class FileUtil:
     ## GENERIC
 
     ## Herein:
-    ## fileformat is in {text, sequence}
-    ## dataformat is in {csv, json}
-    def load_file(self, filename, fileformat, dataformat, **kwargs):
-        if dataformat == "json":
-            return self.load_json_file(filename, fileformat, **kwargs)
-        elif dataformat == "csv":
-            return self.load_csv_file(filename, fileformat, **kwargs)
+    ## file_format is in {text, sequence}
+    ## data_type is in {csv, json}
+    def load_file(self, filename, file_format='sequence', data_type='json', **kwargs):
+        if data_type == "json":
+            return self.load_json_file(filename, file_format, **kwargs)
+        elif data_type == "csv":
+            return self.load_csv_file(filename, file_format, **kwargs)
         else:
-            raise ValueError("Unexpected fileformat {}".format(fileformat))
+            raise ValueError("Unexpected file_format {}".format(file_format))
 
-    def save_file(self, rdd, filename, fileformat, dataformat, **kwargs):
-        if dataformat == "json":
-            return self.save_json_file(rdd, filename, fileformat, **kwargs)
-        elif dataformat == "csv":
-            return self.save_csv_file(rdd, filename, fileformat, **kwargs)
+    def save_file(self, rdd, filename, file_format='sequence', data_type='json', **kwargs):
+        if data_type == "json":
+            return self.save_json_file(rdd, filename, file_format, **kwargs)
+        elif data_type == "csv":
+            return self.save_csv_file(rdd, filename, file_format, **kwargs)
         else:
-            raise ValueError("Unexpected fileformat {}".format(fileformat))
+            raise ValueError("Unexpected file_format {}".format(file_format))
 
     ## JSON
 
@@ -44,38 +44,38 @@ class FileUtil:
             elif len(line_elem) == 1:
                 return '', json.loads(line_elem[0])
 
-    def load_json_file(self, filename, fileformat, separator='\t'):
+    def load_json_file(self, filename, file_format, separator='\t'):
         """options is a dict or something coercible to dict
 returns RDD of <uri, pyjson>
 where pyjson is the python representation of the JSON object (e.g., dict)"""
-        if fileformat == "text":
+        if file_format == "text":
             # each line is <key><tab><json>
             input_rdd = self.sc.textFile(filename).map(lambda x: FileUtil.__parse_json_line(x, separator))
-        elif fileformat == "sequence":
+        elif file_format == "sequence":
             # each element is <key><tab><json>
             input_rdd = self.sc.sequenceFile(filename).mapValues(lambda x: json.loads(x))
         else:
-            raise ValueError("Unexpected fileformat {}".format(fileformat))
+            raise ValueError("Unexpected file_format {}".format(file_format))
         return input_rdd
 
     @staticmethod
     def __dump_as_json(key, value, sep):
         return key + sep + json.dumps(value)
 
-    def save_json_file(self, rdd, filename, fileformat, separator='\t'):
-        if fileformat == "text":
+    def save_json_file(self, rdd, filename, file_format='sequence', separator='\t'):
+        if file_format == "text":
             rdd.map(lambda (k, v): FileUtil.__dump_as_json(k, v, separator).saveAsTextFile(filename))
-        elif fileformat == "sequence":
+        elif file_format == "sequence":
             # whatever it is, key is retained
             rdd.mapValues(lambda x: json.dumps(x)).saveAsSequenceFile(filename)
         else:
-            raise ValueError("Unexpected fileformat {}".format(fileformat))
+            raise ValueError("Unexpected file_format {}".format(file_format))
 
     ## CSV
 
-    def load_csv_file(self, filename, fileformat, separator=','):
+    def load_csv_file(self, filename, file_format, separator=','):
         """returns RDD, each row has all fields as list"""
-        if fileformat == "text":
+        if file_format == "text":
             # http://stackoverflow.com/a/33864015/2077242
             input_rdd = self.sc.textFile(filename)
 
@@ -87,14 +87,14 @@ where pyjson is the python representation of the JSON object (e.g., dict)"""
             parsed_rdd = input_rdd.map(load_csv_record)
             return parsed_rdd
 
-        elif fileformat == "sequence":
-            raise NotImplementedError("Fileformat=sequence, dataformat=csv")
+        elif file_format == "sequence":
+            raise NotImplementedError("File_Format=sequence, data_type=csv")
         else:
-            raise ValueError("Unexpected fileformat {}".format(fileformat))
+            raise ValueError("Unexpected file_format {}".format(file_format))
         return input_rdd
 
-    def save_csv_file(self, rdd, filename, fileformat, separator=',', encoding='utf-8'):
-        if fileformat == "text":
+    def save_csv_file(self, rdd, filename, file_format, separator=',', encoding='utf-8'):
+        if file_format == "text":
             with io.open(filename, 'wb', encoding=encoding) as f:
                 wrtr = csv.writer(f, delimiter=separator)
                 
@@ -104,10 +104,10 @@ where pyjson is the python representation of the JSON object (e.g., dict)"""
                 rdd.foreach(save_csv_record)
                 return filename
 
-        elif fileformat == "sequence":
-            raise NotImplementedError("Fileformat=sequence, dataformat=csv")
+        elif file_format == "sequence":
+            raise NotImplementedError("File_Format=sequence, data_type=csv")
         else:
-            raise ValueError("Unexpected fileformat {}".format(fileformat))
+            raise ValueError("Unexpected file_format {}".format(file_format))
 
     @staticmethod
     def get_json_config(config_spec):
