b844b07b8aee05dcd154a835f7e3cf13693d6955
Andrew Philpot <andrew.philpot@gmail.com>
2015-12-16 08:27:46 -0800

    dict_minus allows multiple keys to remove

--- digSparkUtil/dictUtil.py
+++ digSparkUtil/dictUtil.py
@@ -11,12 +11,13 @@ def as_dict(likeDict):
         except:
             raise TypeError("Can't coerce {} to act like a dict".format(likeDict))
 
-def dict_minus(d, key):
-    """Delete key from dict if exists, returning resulting dict"""
+def dict_minus(d, *keys):
+    """Delete key(s) from dict if exists, returning resulting dict"""
     # make shallow copy
     d = dict(d)
-    try:
-        del d[key]
-    except:
-        pass
+    for key in keys:
+        try:
+            del d[key]
+        except:
+            pass
     return d
