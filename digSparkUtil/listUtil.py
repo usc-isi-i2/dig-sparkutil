340fa1285ab7918ea2442278c18b297fcac70388
Andrew Philpot <andrew.philpot@gmail.com>
2015-12-17 16:19:26 -0800

    flatten lists

--- digSparkUtil/listUtil.py
+++ digSparkUtil/listUtil.py
@@ -5,3 +5,16 @@ def as_list(x):
         return x
     else:
         return [x]
+
+
+# http://stackoverflow.com/a/2158532/2077242
+
+import collections
+
+def flatten(l):
+    for el in l:
+        if isinstance(el, collections.Iterable) and not isinstance(el, basestring):
+            for sub in flatten(el):
+                yield sub
+        else:
+            yield el
