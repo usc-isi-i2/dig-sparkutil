948c64e2ce9d1fcd699c04d02287e6a5b8e0a18f
Andrew Philpot <andrew.philpot@gmail.com>
2015-12-23 14:26:10 -0800

    correct the return value, improve the documentation

--- digSparkUtil/rddUtil.py
+++ digSparkUtil/rddUtil.py
@@ -8,10 +8,13 @@ def limit_rdd(rdd, limit=None, randomSeed=1234):
     if not limit:
         return rdd
     else:
-        # Because take/takeSample collects back to master, can create "task too large" condition
+        # Originally:
         # rdd_ingest = sc.parallelize(rdd_ingest.take(limit))
-        # Instead, generate approximately 'limit' rows
+        # However:
+        # Because take/takeSample collects back to master, can needlessly 
+        # create "task too large" condition
+        # Therefore, instead, sample approximately 'limit' elements
         ratio = float(limit) / rdd.count()
         rdd_limited = rdd.sample(False, ratio, seed=randomSeed)
-        logging.info("RDD limited to {} elements".format(rdd.count()))
+        logging.info("RDD limited to {} elements (requested: {})".format(rdd_limited.count(), limit))
         return rdd_limited
